b88dae4b1f00e43ef34462ff0dabe103b99ac81f
mozdok-oleg <mozdok-oleg@yandex.ru>
2026-05-01 15:32:27 +0300

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REPO_SSH="${REPO_SSH:-git@github.com:mozdok-oleg/portfw.git}"
 APP_DIR="${APP_DIR:-/opt/portfw}"
 BRANCH="${BRANCH:-main}"
-SSH_KEY="${SSH_KEY:-/root/.ssh/portfw_deploy}"
+SSH_KEY="/root/.ssh/portfw_deploy"
 
 if [ "$(id -u)" -ne 0 ]; then
   echo "Run as root"
@@ -14,15 +14,13 @@ fi
 apt-get update -y
 apt-get install -y git
 
-export GIT_SSH_COMMAND="ssh -i ${SSH_KEY} -o IdentitiesOnly=yes"
-
 if [ -d "$APP_DIR/.git" ]; then
   cd "$APP_DIR"
-  git fetch --all --prune
-  git reset --hard "origin/$BRANCH"
+  GIT_SSH_COMMAND="ssh -i ${SSH_KEY} -o IdentitiesOnly=yes" git fetch --all --prune
+  GIT_SSH_COMMAND="ssh -i ${SSH_KEY} -o IdentitiesOnly=yes" git reset --hard "origin/$BRANCH"
 else
   rm -rf "$APP_DIR"
-  git clone -b "$BRANCH" "$REPO_SSH" "$APP_DIR"
+  GIT_SSH_COMMAND="ssh -i ${SSH_KEY} -o IdentitiesOnly=yes" git clone -b "$BRANCH" "$REPO_SSH" "$APP_DIR"
 fi
 
 cd "$APP_DIR"
